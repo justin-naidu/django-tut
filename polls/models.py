ed11692340021bfc00de1aaab3502a05e7647aeb
Justin Naidu <justin@rfc.private.address.invalid.query>
2015-05-13 20:00:32 +0200

    Added was recently publishes function

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -8,6 +11,9 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 
+	def was_published_recently(self):
+		return self.pub_date > timezone.now() - date.timedelta(days=1)
+
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
@@ -15,5 +21,5 @@ class Choice(models.Model):
 
 	def __str__(self):
 		return self.choice_text
-		
+
 
