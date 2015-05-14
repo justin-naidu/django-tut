6251d4aeadd72573a3256b5a2663ffeae4d91193
Justin Naidu <justin@justins-mbp.af.didata.local>
2015-05-14 11:49:50 +0200

    Added details page

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Question, Choice
+from .models import Question, Choice
 
 def index(request):
 	questions = Question.objects.order_by('-pub_date')[:5]
@@ -9,7 +9,12 @@ def index(request):
 	return HttpResponse(output)
 
 def detail(request, question_id):
-	return HttpResponse("You are looking at Question %s" % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+
+	return render(request, "polls/detail.html", {'question': question})
 
 def results(request, question_id):
 	return HttpResponse("Your lookign at the results of Question %s" % question_id)
